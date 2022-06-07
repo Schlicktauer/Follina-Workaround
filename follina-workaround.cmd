41cadf66fdff86fe3a7adb3df60578a3de561746
Schlicktauer <96724858+utzdvcics@users.noreply.github.com>
2022-06-07 12:36:19 +0200

    Create follina-workaround.cmd
    V 20220603

---
+++ follina-workaround.cmd
@@ -0,0 +1,65 @@
+@echo off
+
+setlocal
+
+net session >NUL 2>&1
+
+IF %errorLevel% == 0 (
+	echo File was run as administrator / with elevated permissions. > NUL
+) else (
+	echo File MUSST be executed with elevated privileges.
+	goto :END
+)
+
+mkdir %SystemDrive%\FixIt 2> NUL
+
+set LogFile=%SystemDrive%\FixIt\FixIt.log
+set LockFile=%SystemDrive%\FixIt\_20220603_
+
+if exist %LockFile% goto :END
+
+echo %DATE% %TIME% >> %LogFile%
+
+call :RemoveKey HKCR\ms-msdt %SystemDrive%\FixIt\ms-msdt.reg
+call :RemoveKey HKCR\ms-search %SystemDrive%\FixIt\ms-search.reg
+
+echo %DATE% %TIME% >> %LogFile%
+echo. >> %LogFile%
+
+copy /Y NUL %LockFile% 1> NUL 2> NUL
+
+goto END
+
+:::::::::::::::::::::::::::::::::::::::::::::::
+
+:RemoveKey
+	setlocal
+	set key=%1
+	set file=%2
+
+    %SystemRoot%\system32\reg.exe query %key% 1>NUL 2> NUL
+		 
+	if %errorlevel% == 1 (
+		echo Registry key %key% does not exist >> %LogFile%
+		goto :EOF
+	)
+
+	if not exist %file% (
+		echo %SystemRoot%\system32\reg.exe export %key% %file% >> %LogFile%
+			 %SystemRoot%\system32\reg.exe export %key% %file% >> %LogFile% 2> NUL
+	)
+	
+	if %errorLevel% == 0 (
+		echo %SystemRoot%\system32\reg.exe delete %key% /f >> %LogFile%
+			 %SystemRoot%\system32\reg.exe delete %key% /f 1>> %LogFile% 2>&1
+	) else (
+		echo Error exporting key %key% >> %LogFile%
+	)
+
+:: End of subroutine
+goto :EOF
+
+
+:END
+
+
